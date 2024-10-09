2b15d7626e029bd9ed74e3f4627105dfb854e4d0
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-10-09 16:38:22 -0400

    first commit

---
+++ cookie_taste/__main__.py
@@ -0,0 +1,10 @@
+from cookie_taste.cookie_taste import Cookie_taste
+
+
+def main():
+    chris_app = Cookie_taste()
+    chris_app.launch()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from os import path
+from setuptools import setup
+
+with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name             = 'cookie_taste',
+    version          = '1.0.0',
+    description      = 'A simple plugin to demonstrate a working plugin from cookiecutter-chrisapp',
+    long_description = readme,
+    author           = 'FNNDSC',
+    author_email     = 'dev@babyMRI.org',
+    url              = 'http://wiki',
+    packages         = ['cookie_taste'],
+    install_requires = ['chrisapp'],
+    test_suite       = 'nose.collector',
+    tests_require    = ['nose'],
+    license          = 'MIT',
+    zip_safe         = False,
+    python_requires  = '>=3.6',
+    entry_points     = {
+        'console_scripts': [
+            'cookie_taste = cookie_taste.__main__:main'
+            ]
+        }
+)
